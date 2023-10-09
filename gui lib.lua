addc3e68a30ef4c1eb6c9f6ec082d649cd3146ba
jamiefjdiscord <145333005+jamiefjdiscord@users.noreply.github.com>
2023-10-09 05:15:51 -0700

    Update gui lib.lua

--- gui lib.lua
+++ gui lib.lua
@@ -132,7 +132,7 @@ function libary:CreateMain()
 	Credits.Position = UDim2.new(0.022, 0, 0.835390925, 0)
 	Credits.Size = UDim2.new(0, 118, 0, 40)
 	Credits.Font = Enum.Font.SourceSans
-	Credits.Text = "Made By Spect aka Blukez"
+	Credits.Text = "jamiefj#0000"
 	Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
 	Credits.TextScaled = true
 	Credits.TextSize = 14.000
